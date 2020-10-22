d7b547afa4d27ac8f13aa57da2ff171dc1f6c59f
hamzahskafi <24636307+hamzahskafi@users.noreply.github.com>
2020-10-22 21:53:10 +0300

    Update MovieDetailsModal.js

--- client/app/src/components/MovieDetailsModal.js
+++ client/app/src/components/MovieDetailsModal.js
@@ -20,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     position: "absolute",
     width: 400,
-    backgroundColor: theme.palette.app.gray,
+    backgroundColor: theme.palette.background.paper,
     border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
